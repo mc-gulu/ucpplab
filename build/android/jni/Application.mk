18e30d97904187884870cc331140f2f478b835a5
Gu Lu (home-dev-desktop) <gemfeeling@hotmail.com>
2015-09-20 16:03:48 +0800

    on android, support both GCC and CLang

--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -1,5 +1,17 @@
+
 APP_ABI := armeabi-v7a
-NDK_TOOLCHAIN_VERSION := 4.8
-APP_STL := gnustl_shared
-APP_CPPFLAGS += -std=c++11
-APP_PLATFORM := android-10
+
+APP_CPPFLAGS += -std=c++11 -W#pragma-messages
+
+
+# define this flag to use CLang 3.5 (llvm), otherwise gcc is used
+USE_CLANG := 1
+
+
+ifeq ($(USE_CLANG), 1)
+	NDK_TOOLCHAIN_VERSION := clang
+	APP_STL := c++_static
+else
+	NDK_TOOLCHAIN_VERSION := 4.8
+	APP_STL := gnustl_shared
+endif
--- src/UCppLab.cpp
+++ src/UCppLab.cpp
@@ -1,6 +1,18 @@
 
 #include "UCppLab.h"
 
+#if defined(_MSC_VER)
+#   pragma message ("Compiling with Microsoft Visual C++")
+#endif
+
+#if defined(__clang__)
+#   pragma message ("Compiling with CLang")
+#endif 
+
+#if defined(__GNUC__) && !defined(__clang__)
+#   pragma message ("Compiling with GCC")
+#endif 
+
 void UCppLab::OnStartup() 
 {
 	LOGI(__FUNCTION__);
